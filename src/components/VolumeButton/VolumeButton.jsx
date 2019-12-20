b0c90f45de1408d8073e432574bfcce9d779d7cf
shockolada <shockolada@gmail.com>
2019-12-21 00:56:36 +0200

    volumeIcon created

--- src/components/VolumeButton/VolumeButton.jsx
+++ src/components/VolumeButton/VolumeButton.jsx
@@ -1,5 +1,10 @@
 import React, { PureComponent } from "react";
-import { VolumeUp } from "@material-ui/icons";
+import {
+  VolumeUp,
+  VolumeDown,
+  VolumeMute,
+  VolumeOff
+} from "@material-ui/icons";
 import { withStyles } from "@material-ui/styles";
 import { volumeButtonStyle } from "./volumeButtonStyle";
 import Slider from "@material-ui/core/Slider";
@@ -23,30 +28,45 @@ class VolumeButton extends PureComponent {
   };
 
   handleSliderChange = (e, value) => {
-    this.props.onChange(value)
+    this.props.onChange(value);
   };
 
   render() {
     const { classes } = this.props;
-    const volume = this.props.volume != null ? this.props.volume : 100
+    const volume = this.props.volume != null ? this.props.volume : 100;
+    let volumeIcon = <VolumeUp />;
+    switch (true) {
+      case 0:
+        volumeIcon = <VolumeOff />;
+        break;
+      case (volume < 30):
+        volumeIcon = <VolumeMute />;
+        break;
+      case (volume < 70):
+        volumeIcon = <VolumeDown />;
+        break;
+      default: 
+        volumeIcon = <VolumeUp />;
+    }
     return (
-      <div className={classes.root}
+      <div
+        className={classes.root}
         onMouseEnter={() => this.hoverVolumeButton(true)}
         onMouseLeave={() => this.hoverVolumeButton(false)}
       >
-        <VolumeUp />
+        {volumeIcon}
         {this.state.show && (
-        <div className={classes.sliderContainer}>
-          <Slider
-            name="volume"
-            orientation="vertical"
-            value={volume}
-            onChange={this.handleSliderChange}
-            classes={{
-              root: classes.sliderRoot,
-            }}
-          />
-        </div>
+          <div className={classes.sliderContainer}>
+            <Slider
+              name="volume"
+              orientation="vertical"
+              value={volume}
+              onChange={this.handleSliderChange}
+              classes={{
+                root: classes.sliderRoot
+              }}
+            />
+          </div>
         )}
       </div>
     );
@@ -54,4 +74,3 @@ class VolumeButton extends PureComponent {
 }
 
 export default withStyles(volumeButtonStyle)(VolumeButton);
-
